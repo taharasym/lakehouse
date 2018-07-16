6c5a1adb6b3aae2639da66614bba4a020e1193de
Tim <taharasym@yahoo.com>
2018-07-16 13:46:42 -0500

    demo

---
+++ demo/src/main/java/com/lakehouse/app/LakeHouse.java
@@ -0,0 +1,13 @@
+package com.lakehouse.app;
+
+public class LakeHouse
+{
+
+    public LakeHouse() {
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Our Lake House!!");
+    }
+}
