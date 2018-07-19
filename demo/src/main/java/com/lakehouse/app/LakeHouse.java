4670fb248ca13551a4ffccdd78e0061f3f442169
ABCBSNET\taharasym <taharasym@arkbluecross.com>
2018-07-19 16:13:03 -0500

    test

--- demo/src/main/java/com/lakehouse/app/LakeHouse.java
+++ demo/src/main/java/com/lakehouse/app/LakeHouse.java
@@ -8,6 +8,6 @@ public class LakeHouse
 
     public static void main(String [] args)
     {
-        System.out.println("Our Lake House test!!");
+        System.out.println("Our Lake House tests!!");
     }
 }
